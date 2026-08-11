333d56dc19d0481af7263f65663fc6b948224142
AlMizan-09 <almizan292@gmail.com>
2026-08-11 09:45:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const initSqlJs = require('sql.js');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// CORS block hatane ke liye middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
